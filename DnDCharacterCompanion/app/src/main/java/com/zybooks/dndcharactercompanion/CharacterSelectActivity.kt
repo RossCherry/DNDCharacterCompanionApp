f3011024abb4087a36cb111bb1fda9ab388b1c5a
R1tz02 <jaxsterbaxster2@gmail.com>
2023-11-13 16:15:56 -0600

    change names and frame activity

--- DnDCharacterCompanion/app/src/main/java/com/zybooks/dndcharactercompanion/CharacterSelectActivity.kt
+++ DnDCharacterCompanion/app/src/main/java/com/zybooks/dndcharactercompanion/CharacterSelectActivity.kt
@@ -3,7 +3,7 @@ package com.zybooks.dndcharactercompanion
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CharacterSelectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_character_select)
