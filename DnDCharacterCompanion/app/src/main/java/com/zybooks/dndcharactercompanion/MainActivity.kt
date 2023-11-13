691d9d9735420bacfcc700015bd878dcc2d3d71c
R1tz02 <jaxsterbaxster2@gmail.com>
2023-11-13 16:09:39 -0600

    add project

---
+++ DnDCharacterCompanion/app/src/main/java/com/zybooks/dndcharactercompanion/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zybooks.dndcharactercompanion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_select)
+    }
+}
